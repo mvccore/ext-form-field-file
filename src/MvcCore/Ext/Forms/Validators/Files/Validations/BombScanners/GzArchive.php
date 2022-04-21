1255f7effa28df82896384700ba26dec34923b00
Tom Flídr <tomflidr@gmail.com>
2022-04-21 11:52:47 +0200

    Removed old compatible Exceptions catching.

--- src/MvcCore/Ext/Forms/Validators/Files/Validations/BombScanners/GzArchive.php
+++ src/MvcCore/Ext/Forms/Validators/Files/Validations/BombScanners/GzArchive.php
@@ -134,7 +134,6 @@ class GzArchive implements \MvcCore\Ext\Forms\Validators\Files\Validations\IBomb
 			foreach ($pharFiles as $item) 
 				$this->files[] = $item;
 			$this->index = -1;
-		} catch (\Exception $e) { // backward compatibility
 		} catch (\Throwable $e) {
 		}
 		
